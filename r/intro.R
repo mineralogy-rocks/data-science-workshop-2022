4abeb2de1f4c6461bb0a446d8813592c075f29b6
Liubomyr Gavryliv <liubomyr.gavryliv@gmail.com>
2022-05-19 15:45:01 +0200

    added few other sections

--- r/intro.R
+++ r/intro.R
@@ -1,4 +1,3 @@
-2 + 2
 
 
 test <- read.table('../data/raw_data/mafic_dykes.csv', header=TRUE, sep=',')
@@ -9,3 +8,7 @@ rock_types = unique(test$Rock.Type)
 test
 print('safsa')
 print('something else\nas')
+
+
+
+?print
