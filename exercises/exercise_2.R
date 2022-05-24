ed6762501eb1867bcd6bb4b1b202d0c64e6c80f8
liubomyr.gavryliv <liubomyr.gavryliv@gmail.com>
2022-05-24 23:08:14 +0200

    day 2 WIP

--- exercises/exercise_2.R
+++ exercises/exercise_2.R
@@ -1,6 +1,7 @@
 library(readr)
 library(tidyr)
 library(knitr)
+library(dplyr)
 
 initial_data <-readxl::read_xls('./data/raw_data/Chrysoberyl analysis database.xls')
 
@@ -10,7 +11,8 @@ initial_data_t <- as_tibble(cbind(Sample = names(initial_data)[-1], initial_data
 
 initial_data_t <- 
   initial_data_t %>%
-  mutate_at(vars(-Sample, -Origin, -Reference), as.numeric)
+  mutate_at(vars(-Sample, -Origin, -Reference), as.numeric) %>%
+  mutate_at(vars(-Sample, -Origin, -Reference), .funs=function(x){ replace(x, x<=0, NA) })
 
 # Write your code below this line.
 
